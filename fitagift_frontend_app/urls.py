0b34f85ae342aed0ed9c9ee4ca5d034e532e7c6c
Yariv Katz <yariv@nerdeez.com>
2013-07-15 18:12:48 +0300

    init django app

---
+++ fitagift_frontend_app/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'fitagift_frontend_app.views.home', name='home'),
+    # url(r'^fitagift_frontend_app/', include('fitagift_frontend_app.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
