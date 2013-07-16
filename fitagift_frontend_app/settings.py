f53f00c9a64a8869a16ac732a80a2c570bd32d1b
Yariv Katz <yariv@nerdeez.com>
2013-07-16 17:59:22 +0300

    why the fuck doesnt it work on heroku

--- fitagift_frontend_app/settings.py
+++ fitagift_frontend_app/settings.py
@@ -1,6 +1,8 @@
 # Django settings for fitagift_frontend_app project.
+import os
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
-DEBUG = True
+DEBUG = os.environ['FITAGIFT_ENV_DEBUG'] == 'TRUE'
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -61,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = SITE_ROOT + '/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
