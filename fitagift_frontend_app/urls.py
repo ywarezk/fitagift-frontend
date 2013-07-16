2b42b6a2438cfed9202c768810f99e472fbe1e3c
Yariv Katz <yariv@nerdeez.com>
2013-07-16 19:31:02 +0300

    trying to fix the static files

--- fitagift_frontend_app/urls.py
+++ fitagift_frontend_app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from fitagift_frontend_app.views import spa
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +17,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
