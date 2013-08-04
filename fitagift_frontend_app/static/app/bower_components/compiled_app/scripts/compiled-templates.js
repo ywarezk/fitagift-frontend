22c335d8abc13ebb3ec238bd0bdcf660bf43c271
OpenShift guest <ceo@ykapps.com>
2013-08-04 11:15:04 -0400

    finished graphics for the homepage

--- fitagift_frontend_app/static/app/bower_components/compiled_app/scripts/compiled-templates.js
+++ fitagift_frontend_app/static/app/bower_components/compiled_app/scripts/compiled-templates.js
@@ -6,44 +6,44 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
 function program1(depth0,data) {
   
   
-  data.buffer.push("\n    						<i class=\"icon-home\"></i>\n    						<span>Home</span>        					\n    					");
+  data.buffer.push("\n        						<i class=\"icon-home\"></i>\n        						<span>Home</span>        					\n        					");
   }
 
 function program3(depth0,data) {
   
   
-  data.buffer.push("\n                        <i class=\"icon-info\"></i>\n                        <span>About</span>\n                        ");
+  data.buffer.push("\n                            <i class=\"icon-info\"></i>\n                            <span>About</span>\n                            ");
   }
 
 function program5(depth0,data) {
   
   
-  data.buffer.push("\n                        <i class=\"icon-envelope\"></i>\n                        <span>Contact Us</span>\n                        ");
+  data.buffer.push("\n                            <i class=\"icon-envelope\"></i>\n                            <span>Contact Us</span>\n                            ");
   }
 
-  data.buffer.push("<!--\n\nHolds the html of the application header\nauthor: Yariv Katz\ncopyright: nerdeez.com\nversion: 1.0\n\n-->\n\n<div class=\"_header\" id=\"header\">\n    <div class=\"container\">\n    	<div class=\"row\">\n            <a class=\"pull-left logo\" href=\"/#/\">\n                <img src=");
+  data.buffer.push("<!--\n\nHolds the html of the application header\nauthor: Yariv Katz\ncopyright: nerdeez.com\nversion: 1.0\n\n-->\n\n<div class=\"_header\" id=\"header\">\n    <div class=\"container\">\n    	<div class=\"row\">\n            <div class=\"span12\">\n                <a class=\"pull-left logo\" href=\"/#/\">\n                    <img src=");
   hashTypes = {};
   hashContexts = {};
   data.buffer.push(escapeExpression(helpers.unbound.call(depth0, "view.staticUrl", {hash:{},contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
-  data.buffer.push("img/logo.png>\n                <span>Fitting your gifts</span>\n            </a>\n    	\n    		<div class=\"nav-collapse menu\">\n    			<ul class=\"nav pull-right\" id=\"main-nav\">\n    				<li class=\"nav-icon\">\n    					");
+  data.buffer.push("img/logo.png>\n                    <span>Fitting your gifts</span>\n                </a>\n        	\n        		<div class=\"nav-collapse menu\">\n        			<ul class=\"nav pull-right\" id=\"main-nav\">\n        				<li class=\"nav-icon\">\n        					");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "index", options) : helperMissing.call(depth0, "linkTo", "index", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
-  data.buffer.push("\n    				</li>\n    				<li>\n                        ");
+  data.buffer.push("\n        				</li>\n        				<li>\n                            ");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "about", options) : helperMissing.call(depth0, "linkTo", "about", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
-  data.buffer.push("\n                    </li>\n                    <li>\n                        ");
+  data.buffer.push("\n                        </li>\n                        <li>\n                            ");
   hashTypes = {};
   hashContexts = {};
   options = {hash:{},inverse:self.noop,fn:self.program(5, program5, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
   stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "contact", options) : helperMissing.call(depth0, "linkTo", "contact", options));
   if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
-  data.buffer.push("\n                    </li>\n    				\n    			</ul>\n    			\n    		</div> <!-- /.nav-collapse -->\n            \n    	</div>\n    </div>\n</div>");
+  data.buffer.push("\n                        </li>\n        				\n        			</ul>\n        			\n        		</div> <!-- /.nav-collapse -->\n            </div>\n            \n    	</div>\n    </div>\n</div>");
   return buffer;
   
 });
@@ -150,13 +150,23 @@ function program1(depth0,data) {
 Ember.TEMPLATES["index"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [3,'>= 1.0.0-rc.4'];
 helpers = helpers || Ember.Handlebars.helpers; data = data || {};
-  var buffer = '', hashTypes, hashContexts, escapeExpression=this.escapeExpression;
+  var buffer = '', stack1, stack2, hashContexts, hashTypes, options, self=this, helperMissing=helpers.helperMissing;
 
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("\n                <i class=\"icon-flag-checkered\"></i>\n                START\n                ");
+  }
 
-  data.buffer.push("<h1>Homepage</h1>\n");
-  hashTypes = {};
-  hashContexts = {};
-  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "outlet", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("<div class=\"index\">\n    <div class=\"account-container login stacked\">\n        <div class=\"\">\n            <div class=\"\">\n                <div class=\"explain top-buffer\">\n                    <h1>\n                    Find the perfect gift!\n                    </h1>\n                    <h5>\n                    Click Start, Answer the following questions, and find out the perfect gift for your loved ones.\n                    </h5>\n                </div>\n                <div class=\"button-container top-buffer\">\n                ");
+  hashContexts = {'classNames': depth0};
+  hashTypes = {'classNames': "STRING"};
+  options = {hash:{
+    'classNames': ("btn btn-large btn-primary btn-block btn-big-block")
+  },inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers.linkTo),stack1 ? stack1.call(depth0, "index", options) : helperMissing.call(depth0, "linkTo", "index", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("\n                </div>\n            </div>\n        </div>\n    </div>\n</div>");
   return buffer;
   
 });
