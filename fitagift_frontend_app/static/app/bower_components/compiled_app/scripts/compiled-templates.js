8eba732e9ed1bfadc72627c87e62dbcd9d535976
OpenShift guest <ceo@ykapps.com>
2013-08-04 11:30:19 -0400

    styles for the flatpages

--- fitagift_frontend_app/static/app/bower_components/compiled_app/scripts/compiled-templates.js
+++ fitagift_frontend_app/static/app/bower_components/compiled_app/scripts/compiled-templates.js
@@ -126,23 +126,23 @@ helpers = helpers || Ember.Handlebars.helpers; data = data || {};
 function program1(depth0,data) {
   
   var buffer = '', stack1, hashContexts, hashTypes;
-  data.buffer.push("\n                <div class=\"hero-unit\">\n                    ");
+  data.buffer.push("\n                    <div class=\"top-buffer\">\n                        ");
   hashContexts = {'unescaped': depth0};
   hashTypes = {'unescaped': "STRING"};
   stack1 = helpers._triageMustache.call(depth0, "html", {hash:{
     'unescaped': ("true")
   },contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n                </div>\n            ");
+  data.buffer.push("\n                    </div>\n                ");
   return buffer;
   }
 
-  data.buffer.push("<div class=\"flatpage\">\n    <div class=\"container\">\n        <div class=\"row\">\n            <div class=\"span12\">\n            ");
+  data.buffer.push("<div class=\"flatpage\">\n    <div class=\"account-container login stacked\">\n        <div class=\"container\">\n            <div class=\"row\">\n                <div class=\"span12 content\">\n                ");
   hashTypes = {};
   hashContexts = {};
   stack1 = helpers.each.call(depth0, "controller", {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data});
   if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
-  data.buffer.push("\n            </div>\n        </div>\n    </div>\n</div>");
+  data.buffer.push("\n                </div>\n            </div>\n        </div>\n    </div>\n</div>");
   return buffer;
   
 });
